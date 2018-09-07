59e60bf2c8adb7af559fef2b74fdc240321b0dcb
Esko Salaka <esko.salaka@aalto.fi>
2018-09-07 13:25:22 +0300

    tehtävät 4.4-4.5

--- Osa 4/utils/list_helper.js
+++ Osa 4/utils/list_helper.js
@@ -2,6 +2,30 @@ const dummy = blogs => {
   return 1;
 };
 
+const totalLikes = blogs => {
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce((sum, blog) => sum + blog.likes, 0);
+};
+
+const favoriteBlog = blogs => {
+  var favourite = null;
+
+  if (blogs === null) {
+    return null;
+  }
+
+  blogs.forEach(blog => {
+    if (favourite === null || favourite.likes < blog.likes) {
+      favourite = blog;
+    }
+  });
+
+  return favourite;
+};
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 };
