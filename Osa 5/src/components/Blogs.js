4b3f8d5bf9d45efe144dbad7a6c7ffaf077562ce
Esko Salaka <esko.salaka@aalto.fi>
2018-12-08 14:20:46 +0200

    tehtävä 5.6

--- Osa 5/src/components/Blogs.js
+++ Osa 5/src/components/Blogs.js
@@ -1,11 +1,61 @@
 import React from 'react';
+class Blog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: false
+    };
 
-const BlogLine = ({ blog }) => {
+    this.toggleExpanded = this.toggleExpanded.bind(this);
+  }
+
+  toggleExpanded = () => {
+    this.setState({ expanded: !this.state.expanded });
+  };
+
+  render() {
+    const blogStyle = {
+      paddingTop: 5,
+      paddingLeft: 2,
+      border: 'solid',
+      borderWidth: 3,
+      marginBottom: 5
+    };
+
+    return (
+      <div style={blogStyle}>
+        <BlogTitle
+          blog={this.props.blog}
+          handleTitleClick={this.toggleExpanded}
+        />
+        {this.state.expanded ? <HiddenLines blog={this.props.blog} /> : null}
+      </div>
+    );
+  }
+}
+
+const BlogTitle = ({ blog, handleTitleClick }) => {
   return (
-    <tr>
-      <td>{blog.title}</td>
-      <td>{blog.author}</td>
-    </tr>
+    <p onClick={handleTitleClick}>
+      {blog.title} {blog.author}
+    </p>
+  );
+};
+
+const HiddenLines = ({ blog, handleLikeButtonClick }) => {
+  return (
+    <div>
+      <a href={'//' + blog.url}>{blog.url}</a>
+
+      <p>
+        {' '}
+        {blog.likes} Likes{' '}
+        <button type="like" onClick={handleLikeButtonClick}>
+          Like
+        </button>
+      </p>
+      <p> Added by {blog.user.name}</p>
+    </div>
   );
 };
 
@@ -13,13 +63,11 @@ const Blogs = ({ blogs }) => {
   return (
     <div>
       <h2>Blogs</h2>
-      <table>
-        <tbody>
-          {blogs.map(b => (
-            <BlogLine key={b._id} blog={b} />
-          ))}
-        </tbody>
-      </table>
+      <div>
+        {blogs.map(b => (
+          <Blog key={b._id} blog={b} />
+        ))}
+      </div>
     </div>
   );
 };
