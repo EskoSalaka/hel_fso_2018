73e4d75cb1bd5a9465312de3ff6aaddff4171613
Esko Salaka <esko.salaka@aalto.fi>
2018-12-11 11:02:31 +0200

    tests done --> 5.16

--- Osa 5/5.1-5.16/src/components/Blog.test.js
+++ Osa 5/5.1-5.16/src/components/Blog.test.js
@@ -6,25 +6,49 @@ describe.only('<Blog />', () => {
   const blog = {
     likes: 10,
     author: 'Joel Spolsky',
-    title: 'The Joel Test: 12 Steps to Better Code'
+    title: 'The Joel Test: 12 Steps to Better Code',
+    url: 'www.microsoft.com'
   };
 
   const mockAddLikeHandler = jest.fn();
   const mockDeleteHandler = jest.fn();
   let blogComponent;
+  let hiddenStuff;
+  let titleLine;
 
   beforeEach(() => {
     blogComponent = shallow(<Blog blog={blog} />);
   });
 
   it('default renders only title and author of Blog', () => {
-    const titleLine = blogComponent
+    titleLine = blogComponent
       .find('BlogTitle')
-      .dive()
+      .shallow()
       .find('.titleLine');
-    const hiddenStuff = blogComponent.find('.togglableContent');
-    console.log(titleLine.debug());
+
+    hiddenStuff = blogComponent.find('HiddenLines');
 
     expect(titleLine.text()).toContain(blog.title);
+    expect(titleLine.text()).toContain(blog.author);
+    expect(hiddenStuff.get(0)).toBeFalsy();
+  });
+
+  it('hidden lines will show after pressing the title', () => {
+    titleLine = blogComponent
+      .find('BlogTitle')
+      .shallow()
+      .find('.titleLine');
+
+    titleLine.at(0).simulate('click');
+
+    hiddenStuff = blogComponent.find('HiddenLines');
+
+    expect(titleLine.text()).toContain(blog.title);
+    expect(titleLine.text()).toContain(blog.author);
+    expect(hiddenStuff.get(0)).not.toBeFalsy();
+
+    hiddenStuff = hiddenStuff.shallow();
+    expect(hiddenStuff.text()).toContain(blog.likes);
+    expect(hiddenStuff.text()).toContain(blog.url);
   });
 });
