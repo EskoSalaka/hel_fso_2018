ab445f205a5f4f254653f244b8706678b820c688
Esko Salaka <esko.salaka@aalto.fi>
2018-12-14 12:53:58 +0200

    tehtävä 6.22

--- Osa 6/6.16-6.23/src/App.js
+++ Osa 6/6.16-6.23/src/App.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom'
-import { ListGroup, ListGroupItem } from 'react-bootstrap'
+import {
+  ListGroup,
+  ListGroupItem,
+  Grid,
+  Row,
+  Col,
+  Image
+} from 'react-bootstrap'
 
 const Menu = ({}) => {
   const menuStyle = {
@@ -82,23 +89,44 @@ const Notification = ({ message }) => {
 
 const About = () => (
   <div>
-    <h2>About anecdote app</h2>
-    <p>According to Wikipedia:</p>
-
-    <em>
-      An anecdote is a brief, revealing account of an individual person or an
-      incident. Occasionally humorous, anecdotes differ from jokes because their
-      primary purpose is not simply to provoke laughter but to reveal a truth
-      more general than the brief tale itself, such as to characterize a person
-      by delineating a specific quirk or trait, to communicate an abstract idea
-      about a person, place, or thing through the concrete details of a short
-      narrative. An anecdote is "a story with a point."
-    </em>
-
-    <p>
-      Software engineering is full of excellent anecdotes, at this app you can
-      find the best and add more.
-    </p>
+    <Grid>
+      <Row>
+        <Col xs={12}>
+          <h2>About anecdote app</h2>
+        </Col>
+        <Col xs={8}>
+          <Col>
+            <p>According to Wikipedia:</p>
+          </Col>
+          <Col>
+            <em>
+              An anecdote is a brief, revealing account of an individual person
+              or an incident. Occasionally humorous, anecdotes differ from jokes
+              because their primary purpose is not simply to provoke laughter
+              but to reveal a truth more general than the brief tale itself,
+              such as to characterize a person by delineating a specific quirk
+              or trait, to communicate an abstract idea about a person, place,
+              or thing through the concrete details of a short narrative. An
+              anecdote is "a story with a point."
+            </em>
+          </Col>
+          <Col>
+            <br />
+            <p>
+              Software engineering is full of excellent anecdotes, at this app
+              you can find the best and add more.
+            </p>
+          </Col>
+        </Col>
+        <Col xs={4}>
+          <Image
+            src="https://sdtimes.com/wp-content/uploads/2015/05/GOSLINGJAMES.jpg"
+            headers="80%"
+            width="80%"
+          />
+        </Col>
+      </Row>
+    </Grid>
   </div>
 )
 
